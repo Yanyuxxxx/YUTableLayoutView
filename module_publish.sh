5a0b1b9a7964cc62fa4792f5f3cb3a966489ed85
Yanyuxxxx <yuyanyu@haodf.com>
2020-04-26 19:57:55 +0800

    0.0.4

--- module_publish.sh
+++ module_publish.sh
@@ -27,6 +27,7 @@ echo -e "\n开始构建二进制包"
 pod bin archive $pod_name.podspec --sources=$sources
 if [ $? -ne 0 ]; then
     echo -e "\033[31m构建二进制包失败 \033[0m" 
+    exit 1
 else
     echo -e "\033[32m构建二进制包成功 \033[0m"
 fi
@@ -36,10 +37,13 @@ fi
 echo -e "\n开始上传二进制包"
 echo "开始清除"
 curl -X 'DELETE' "${binary_server_path}/${pod_name}/${pod_version}" -O -J
+rm -f ./${pod_version}
 echo "清除成功"
 curl $binary_server_path -F "name=${pod_name}" -F "version=${pod_version}" -F "file=@./${pod_name}.framework.zip"
+rm -f ./${pod_name}.framework.zip
 if [ $? -ne 0 ]; then
     echo -e "\033[31m\n上传二进制包失败 \033[0m" 
+    exit 1
 else
     echo -e "\033[32m\n上传二进制包成功 \033[0m"
 fi
@@ -50,6 +54,7 @@ echo -e "\n开始验证二进制podspec"
 pod bin spec lint --binary "${pod_name}.binary.podspec.json" --sources=$sources --allow-warnings
 if [ $? -ne 0 ]; then
     echo -e "\033[31m验证二进制podspec失败 \033[0m" 
+    exit 1
 else
     echo -e "\033[32m验证二进制podspec成功 \033[0m"
 fi
@@ -59,6 +64,7 @@ echo -e "\n开始发布二进制组件"
 pod bin repo push --binary "${pod_name}.binary.podspec.json" --sources=$sources --allow-warnings
 if [ $? -ne 0 ]; then
     echo -e "\033[31m发布二进制组件失败 \033[0m" 
+    exit 1
 else
     echo -e "\033[32m发布二进制组件成功 \033[0m"
 fi
@@ -69,6 +75,7 @@ echo -e "\n开始验证源码podspec"
 pod bin spec lint --binary "${pod_name}.podspec" --sources=$sources --allow-warnings
 if [ $? -ne 0 ]; then
     echo -e "\033[31m验证源码podspec失败 \033[0m" 
+    exit 1
 else
     echo -e "\033[32m验证源码podspec成功 \033[0m"
 fi
@@ -78,13 +85,11 @@ echo -e "\n开始发布源码组件"
 pod bin repo push --binary "${pod_name}.podspec" --sources=$sources --allow-warnings
 if [ $? -ne 0 ]; then
     echo -e "\033[31m发布源码组件失败 \033[0m" 
+    exit 1
 else
     echo -e "\033[32m发布源码组件成功 \033[0m"
 fi
 
-
-# 清理现场
-rm -f ./${pod_name}.framework.zip
 
 
 
